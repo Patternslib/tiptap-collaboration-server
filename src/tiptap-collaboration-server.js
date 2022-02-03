209b54a5a6fc60333f8bb2a22278973d0d49eec6
Johannes Raggam <thetetet@gmail.com>
2022-02-03 13:54:39 +0100

    first iteration: loading document, null authentication

--- src/tiptap-collaboration-server.js
+++ src/tiptap-collaboration-server.js
@@ -1,27 +1,96 @@
-import "regenerator-runtime/runtime"; // needed for ``await`` support
-import Base from "@patternslib/patternslib/src/core/base";
-import Parser from "@patternslib/patternslib/src/core/parser";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import { Server } from "@hocuspocus/server";
+import { TiptapTransformer } from "@hocuspocus/transformer";
+import { readFileSync } from "fs";
+import fetch from "node-fetch";
+import { generateJSON } from "@tiptap/html";
 
-export const parser = new Parser("tiptap-collaboration-server");
-parser.addArgument("example-option", [1, 2, 3]);
+const url = "http://info.cern.ch/hypertext/WWW/TheProject.html";
 
-export default Base.extend({
-    name: "tiptap-collaboration-server",
-    trigger: ".tiptap-collaboration-server",
+const server = Server.configure({
+    port: 1234,
 
-    async init() {
-        this.options = parser.parse(this.el, this.options);
+    async __onChange(data) {
+        const prosemirrorJSON = TiptapTransformer.fromYdoc(data.document);
+        console.log(JSON.stringify(prosemirrorJSON));
+        console.log(
+            `Document ${data.documentName} changed by ${data.context.user.name}`
+        );
+    },
 
-        // Just an example!
-        // eslint-disable-next-line no-unused-vars
-        const $ = (await import("jquery")).default; // try to avoid jQuery.
+    async onLoadDocument(data) {
+        // The tiptap collaboration extension uses shared types of a single y-doc
+        // to store different fields in the same document.
+        // The default field in tiptap is simply called 'default'
+        const fieldName = "default";
 
-        // Just an example!
-        // eslint-disable-next-line no-unused-vars
-        const example_option = this.options.exampleOption;
+        // Check if the given field already exists in the given y-doc.
+        // Important: Only import a document if it doesn't exist in the primary data storage!
+        if (!data.document.isEmpty(fieldName)) {
+            return;
+        }
 
-        // Just an example!
-        // And completly useless.
-        document.body.innerHTML = "<p>hello.</p>";
+        // Get the document from somwhere. In a real world application this would
+        // probably be a database query or an API call
+
+        let response;
+        try {
+            response = await fetch(url, {
+                method: "GET",
+                //mode: "cors",
+                //headers: {
+                //    "Accept": "application/json",
+                //    "Content-Type": "application/json",
+                //},
+            });
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+        const result = await response.text();
+        console.log(result);
+
+        const prosemirrorJSON = generateJSON(result, [
+            Document.default,
+            Paragraph.default,
+            Text.default,
+        ]);
+        console.log(prosemirrorJSON);
+
+        //const prosemirrorJSON = JSON.parse(
+        //    readFileSync(`/path/to/your/documents/${data.documentName}.json`) || "{}"
+        //);
+
+        //const prosemirrorJSON = JSON.parse(`
+        //{"default":{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"asdasdasd"}]},{"type":"paragraph","content":[{"type":"text","text":"asdasdl"}]},{"type":"paragraph"},{"type":"paragraph"},{"type":"paragraph","content":[{"type":"text","text":"asdasd"}]},{"type":"paragraph"},{"type":"paragraph"},{"type":"paragraph","content":[{"type":"text","text":"asddddsad"}]},{"type":"paragraph"},{"type":"paragraph"}]}}
+        //      `);
+
+        // Convert the editor format to a y-doc. The TiptapTransformer requires you to pass the list
+        // of extensions you use in the frontend to create a valid document
+        return TiptapTransformer.toYdoc(prosemirrorJSON, fieldName, [
+            Document.default,
+            Paragraph.default,
+            Text.default,
+        ]);
+    },
+
+    async onAuthenticate({ token }) {
+        console.log(token);
+        // Example test if a user is authenticated
+        if (false && token !== "super-secret-token") {
+            throw new Error("Not authorized!");
+        }
+
+        // You can set contextual data to use it in other hooks
+        return {
+            user: {
+                id: 1234,
+                name: "John",
+            },
+        };
     },
 });
+
+server.listen();
